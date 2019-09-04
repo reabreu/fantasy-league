7ffade94503d304ecddf1316d6badc3befdd7830
Ruben Freitas <emanuelfreitas477@gmail.com>
2019-09-04 20:19:34 +0100

    build: add kubernetes prod setup

--- gcloud/deploy-prod.sh
+++ gcloud/deploy-prod.sh
@@ -2,7 +2,6 @@
 
 set -e
 
-docker build -t eu.gcr.io/${PROJECT_PROD}/${NGINX_IMAGE}:$TRAVIS_COMMIT ./nginx
 docker build -t eu.gcr.io/${PROJECT_PROD}/${ADMIN_IMAGE}:$TRAVIS_COMMIT ./packages/admin
 docker build -t eu.gcr.io/${PROJECT_PROD}/${CLIENT_IMAGE}:$TRAVIS_COMMIT ./packages/client
 docker build -t eu.gcr.io/${PROJECT_PROD}/${API_IMAGE}:$TRAVIS_COMMIT ./packages/api
@@ -11,16 +10,19 @@ echo $GCLOUD_SERVICE_KEY_PROD | base64 --decode -i > ${HOME}/gcloud-service-key.
 gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
 
 gcloud --quiet config set project $PROJECT_PROD
-# gcloud --quiet config set container/cluster $CLUSTER
+gcloud --quiet config set container/cluster $CLUSTER
 gcloud --quiet config set compute/zone ${ZONE}
-# gcloud --quiet container clusters get-credentials $CLUSTER
+gcloud --quiet container clusters get-credentials $CLUSTER
 
-gcloud docker -- push eu.gcr.io/${PROJECT_PROD}/${NGINX_IMAGE}
 gcloud docker -- push eu.gcr.io/${PROJECT_PROD}/${ADMIN_IMAGE}
 gcloud docker -- push eu.gcr.io/${PROJECT_PROD}/${CLIENT_IMAGE}
 gcloud docker -- push eu.gcr.io/${PROJECT_PROD}/${API_IMAGE}
 
-yes | gcloud beta container images add-tag eu.gcr.io/${PROJECT_PROD}/${NGINX_IMAGE}:$TRAVIS_COMMIT eu.gcr.io/${PROJECT_PROD}/${NGINX_IMAGE}:latest
 yes | gcloud beta container images add-tag eu.gcr.io/${PROJECT_PROD}/${ADMIN_IMAGE}:$TRAVIS_COMMIT eu.gcr.io/${PROJECT_PROD}/${ADMIN_IMAGE}:latest
 yes | gcloud beta container images add-tag eu.gcr.io/${PROJECT_PROD}/${CLIENT_IMAGE}:$TRAVIS_COMMIT eu.gcr.io/${PROJECT_PROD}/${CLIENT_IMAGE}:latest
 yes | gcloud beta container images add-tag eu.gcr.io/${PROJECT_PROD}/${API_IMAGE}:$TRAVIS_COMMIT eu.gcr.io/${PROJECT_PROD}/${API_IMAGE}:latest
+
+kubectl apply -f k8s
+kubectl set image deployments/admin-deployment admin=eu.gcr.io/${PROJECT_PROD}/${ADMIN_IMAGE}:$TRAVIS_COMMIT
+kubectl set image deployments/client-deployment client=eu.gcr.io/${PROJECT_PROD}/${CLIENT_IMAGE}:$TRAVIS_COMMIT
+kubectl set image deployments/api-deployment api=eu.gcr.io/${PROJECT_PROD}/${API_IMAGE}:$TRAVIS_COMMIT
